6ba3d9814ca2f8a349619f2d61c952d962a4f072
JIElite <ita3051@gmail.com>
2015-11-17 20:30:56 +0800

    add Stores, StoreComment, Tag to models.py

--- mysite/models.py
+++ mysite/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db import migrations
+
 
 class Users(models.Model):
     username = models.CharField(max_length=32)
@@ -11,5 +13,49 @@ class Users(models.Model):
     class Meta:
         ordering = ['created']
 
-    def __unicode__(self):
+    def __str__(self):
         return self.username
+
+
+class Stores(models.Model):
+    name = models.CharField(max_length=32)
+    good = models.IntegerField(null=True, blank=True)
+    bad = models.IntegerField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    location = models.TextField(null=True, blank=True)
+    fan_page = models.URLField(null=True, blank=True)
+    website = models.URLField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['good']
+
+    def __str__(self):
+        return self.name
+
+
+class StoreComment(models.Model):
+    description = models.TextField()
+    good = models.IntegerField(null=True, blank=True)
+    bad = models.IntegerField(null=True, blank=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+
+    store = models.ForeignKey(Stores)
+    user = models.OneToOneField(Users)
+    
+    def __str__(self):
+        return self.description
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField()
+
+    stores = models.ManyToManyField(Stores)
+    # lists = models.ManyToManyField(Lists)
+
+    def __str__(self):
+        return self.name
+
+
+
