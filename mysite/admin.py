c9467557db4d77fdf8a987a40b61a89b15f00724
JIElite <ita3051@gmail.com>
2015-11-17 22:36:05 +0800

    intital admin.py create superuser id:eat, pw:eat

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Users
+from mysite.models import *
+
+class StoresAdmin(admin.ModelAdmin):
+    list_display = ('name','description')
+    search_fields = ('name',)
+
+class TagsAdmin(admin.ModelAdmin):
+    list_display = ('name','description')
+
 
 # Register your models here.
+admin.site.register(Stores,StoresAdmin)
+admin.site.register(Tags,TagsAdmin)
 admin.site.register(Users)
