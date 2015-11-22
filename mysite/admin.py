56cb326630240304f2a558ea10c918794a7e62d1
JIElite <ita3051@gmail.com>
2015-11-22 15:55:41 +0800

    update admin

--- mysite/admin.py
+++ mysite/admin.py
@@ -7,9 +7,24 @@ class StoresAdmin(admin.ModelAdmin):
 
 class TagsAdmin(admin.ModelAdmin):
     list_display = ('name','description')
+    search_fields = ('name',)
+
+class StoreCommentAdmin(admin.ModelAdmin):
+    list_display = ('store','user','description','good','bad')
+    search_fields = ('store',)
+
+class ListsAdmin(admin.ModelAdmin):
+    list_display = ( 'user','name','description')
+    search_fields = ('user',)
 
 
 # Register your models here.
-admin.site.register(Stores,StoresAdmin)
-admin.site.register(Tags,TagsAdmin)
 admin.site.register(Users)
+admin.site.register(Stores, StoresAdmin)
+admin.site.register(Tags, TagsAdmin)
+admin.site.register(StoreComment, StoreCommentAdmin)
+admin.site.register(Lists, ListsAdmin)
+admin.site.register(StoreLike)
+admin.site.register(StoreDislike)
+admin.site.register(ListLike)
+admin.site.register(ListDislike)
