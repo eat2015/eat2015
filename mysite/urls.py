6db6f287a28ddc9815ed6140fdb64d242f7de349
JIElite <ita3051@gmail.com>
2015-12-01 21:28:55 +0800

    update test function to create list

--- mysite/urls.py
+++ mysite/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     url(r'^searchstore$', search_store),
     url(r'^createlist$', createlist),
     url(r'^searchlist$', searchlist),
+    url(r'^createlistsubmit$', create_food_list),
 ]
 
