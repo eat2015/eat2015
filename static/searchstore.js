612a1b3d8dc4cbb46fee46d27bf8c8c18448d6e3
Woody Wu <Woody Wu>
2016-01-10 10:59:47 +0800

    like ajax

--- static/searchstore.js
+++ static/searchstore.js
@@ -11,13 +11,61 @@ $(document).ready(function(){
 		console.log($(this).parent().children(".likebutton")[0].style);
 	});
 	$(".likebutton").click(function(){
+		var button = $(this);
 		if($(this).get(0).style.color == ""){
 			$(this).get(0).style.color = "red";
 			if($(this).parent().children(".dislikebutton")[0].style.color == "black"){
 				$(this).parent().children(".dislikebutton")[0].style.color = "";
 			}
+			console.log(button.find('b').html());
+			button.find('b').html(parseInt(button.find('b').html())+1);
 		}else if($(this).get(0).style.color == "red"){
 			$(this).get(0).style.color = "";
+		}
+		if(button.data('type') == "store"){
+			$.ajax({
+				url : "likestore",
+				type : "POST",
+				data : {storeid:button.data('storeid')},
+				datatype:'json',
+				success : function(data) {
+					if(data == 0){
+						button.get(0).style.color = 'red';
+						button.find('b').html(parseInt(button.find('b').html())+1);
+					}else if(data == 1){
+						button.get(0).style.color = '';
+						button.find('b').html(parseInt(button.find('b').html())-1);
+					}else{
+
+					}
+				},
+				error : function(xhr,errmsg,err) {
+						console.log(xhr.status + ": " + xhr.responseText);
+					} 
+				});
+			};
+		}else if(button.data('type') == "storecomment"){
+			$.ajax({
+				url : "likestorecomment",
+				type : "POST",
+				data : {storeid:button.data('storeid')},
+				datatype:'json',
+				success : function(data) {
+					if(data == 0){
+						button.get(0).style.color = 'red';
+						button.find('b').html(parseInt(button.find('b').html())+1);
+					}else if(data == 1){
+						button.get(0).style.color = '';
+						button.find('b').html(parseInt(button.find('b').html())-1);
+					}else{
+
+					}
+				},
+				error : function(xhr,errmsg,err) {
+						console.log(xhr.status + ": " + xhr.responseText);
+					} 
+				});
+			};
 		}
 	});
 	$("#submitreplybutton").click(function(){
