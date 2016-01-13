41f70a54974d366b457110ea9d230e217c4b4e20
Woody Wu <Woody Wu>
2016-01-13 14:13:29 +0800

    another button

--- static/searchstore.js
+++ static/searchstore.js
@@ -38,6 +38,49 @@ $(document).ready(function(){
 				$(this).get(0).style.color = "black";
 				if($(this).parent().children(".likebutton")[0].style.color == "red"){
 					$(this).parent().children(".likebutton")[0].style.color = "";
+					if(button.data('type') == "store"){
+						$.ajax({
+							url : "likestore",
+							type : "POST",
+							data : {storeid:button.data('storeid')},
+							datatype:'json',
+							success : function(data) {
+								if(data == 0){
+									button.get(0).style.color = 'red';
+									button.find('b').html(parseInt(button.find('b').html())+1);
+								}else if(data == 1){
+									button.get(0).style.color = '';
+									button.find('b').html(parseInt(button.find('b').html())-1);
+								}else{
+
+								}
+							},
+							error : function(xhr,errmsg,err) {
+									console.log(xhr.status + ": " + xhr.responseText);
+								} 
+							});
+					}else if(button.data('type') == "storecomment"){
+						$.ajax({
+							url : "likestorecomment",
+							type : "POST",
+							data : {storecommentid:button.data('storecommentid')},
+							datatype:'json',
+							success : function(data) {
+								if(data == 0){
+									button.get(0).style.color = 'red';
+									button.find('b').html(parseInt(button.find('b').html())+1);
+								}else if(data == 1){
+									button.get(0).style.color = '';
+									button.find('b').html(parseInt(button.find('b').html())-1);
+								}else{
+
+								}
+							},
+							error : function(xhr,errmsg,err) {
+									console.log(xhr.status + ": " + xhr.responseText);
+								} 
+							});
+					}
 				}
 			}else if($(this).get(0).style.color == "black"){
 				$(this).get(0).style.color = "";
@@ -93,6 +136,49 @@ $(document).ready(function(){
 				$(this).get(0).style.color = "red";
 				if($(this).parent().children(".dislikebutton")[0].style.color == "black"){
 					$(this).parent().children(".dislikebutton")[0].style.color = "";
+					if(button.data('type') == "store"){
+						$.ajax({
+							url : "likestore",
+							type : "POST",
+							data : {storeid:button.data('storeid')},
+							datatype:'json',
+							success : function(data) {
+								if(data == 0){
+									button.get(0).style.color = 'red';
+									button.find('b').html(parseInt(button.find('b').html())+1);
+								}else if(data == 1){
+									button.get(0).style.color = '';
+									button.find('b').html(parseInt(button.find('b').html())-1);
+								}else{
+
+								}
+							},
+							error : function(xhr,errmsg,err) {
+									console.log(xhr.status + ": " + xhr.responseText);
+								} 
+							});
+					}else if(button.data('type') == "storecomment"){
+						$.ajax({
+							url : "likestorecomment",
+							type : "POST",
+							data : {storecommentid:button.data('storecommentid')},
+							datatype:'json',
+							success : function(data) {
+								if(data == 0){
+									button.get(0).style.color = 'red';
+									button.find('b').html(parseInt(button.find('b').html())+1);
+								}else if(data == 1){
+									button.get(0).style.color = '';
+									button.find('b').html(parseInt(button.find('b').html())-1);
+								}else{
+
+								}
+							},
+							error : function(xhr,errmsg,err) {
+									console.log(xhr.status + ": " + xhr.responseText);
+								} 
+							});
+					}
 				}
 			}else if($(this).get(0).style.color == "red"){
 				$(this).get(0).style.color = "";
